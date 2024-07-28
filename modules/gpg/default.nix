a4a9cc90ca7fe8d33970a5983a9ad30af8c49ed1
Samuel Carey <sam@scarey.me>
2024-07-28 03:01:23 -0400

    feat: import my public key

--- modules/gpg/default.nix
+++ modules/gpg/default.nix
@@ -1,17 +1,31 @@
 { config, lib, pkgs, ... }:
 
+let
+  scareyGpg = pkgs.fetchurl {
+    url = "https://scarey.me/scarey.gpg";
+    sha256 = "1waqh8slipq31vfxzg2lawbbrxqnzdiyrz6hrg137i6nlvn48925";
+  };
+in
 {
   options = with lib; {
     scarey.home.gpg.enable = mkEnableOption "Enable GnuPG configuration";
   };
 
   config = lib.mkIf config.scarey.home.gpg.enable {
-    programs.gpg.enable = true;
-  
+    programs.gpg = {
+      enable = true;
+      publicKeys = [
+        {
+          source = "${scareyGpg}";
+          trust = "ultimate";
+        }
+      ];
+    };
+
     services.gpg-agent = lib.mkIf pkgs.stdenv.isLinux {
       enable = true;
       enableZshIntegration = true;
-      pinentryPackage = pkgs.pinentry-tty;
+      pinentryPackage = pkgs.pinentry-gnome3;
       
       enableSshSupport = true;
       sshKeys = [
