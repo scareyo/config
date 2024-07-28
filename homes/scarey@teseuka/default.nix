b3b16739a4315994e67de78afd95624459932937
Samuel Carey <sam@scarey.me>
2024-07-28 03:03:07 -0400

    feat: teseuka now runs nixos

--- homes/scarey@teseuka/default.nix
+++ homes/scarey@teseuka/default.nix
@@ -13,18 +13,15 @@
       enable = true;
       name = "Samuel Carey";
       email = "sam@scarey.me";
-      gpgPath = "/mnt/c/Users/scarey/scoop/apps/gpg4win/current/GnuPG/bin/gpg.exe";
     };
 
+    firefox.enable = true;
+    fonts.enable = true;
+    gpg.enable = true;
+    kitty.enable = true;
     nvim.enable = true;
     zsh.enable = true;
   };
 
-  home.activation.windowsTools = config.lib.dag.entryAfter ["writeBoundary"] ''
-    mkdir -p ~/bin
-    ln -sf /mnt/c/Windows/System32/OpenSSH/ssh.exe ~/bin/ssh
-    ln -sf /mnt/c/Users/scarey/scoop/apps/gpg4win/current/GnuPG/bin/gpg.exe ~/bin/gpg
-  '';
-  
   home.stateVersion = "24.05";
 }
