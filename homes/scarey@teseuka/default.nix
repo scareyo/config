1eec41a35a89b21910a86debe5a8e0c55551334a
Samuel Carey <sam@scarey.me>
2026-01-11 15:37:12 -0500

    feat: override vesktop with venbind feature branch

--- homes/scarey@teseuka/default.nix
+++ homes/scarey@teseuka/default.nix
@@ -22,9 +22,49 @@
     osu-lazer-bin
     prismlauncher
     runelite
-    vesktop
     vintagestory
     vlc
+
+    (vesktop.overrideAttrs (finalAttrs: previousAttrs: {
+      src = fetchFromGitHub {
+        owner = "tuxinal";
+        repo = "Vesktop";
+        rev = "main";
+        hash = "sha256-7u3kmpgCWCj+0sSxCg8cylulWVv8oAW6H5jy8f4o+tQ=";
+      };
+      pnpmDeps = fetchPnpmDeps {
+        inherit (finalAttrs) pname version src patches;
+        pnpm = pnpm_10;
+        fetcherVersion = 2;
+        hash = "sha256-TDgg9bDuRoTt/QaN+zF8W1ip3vzte203+kgBHX1LJlI=";
+      };
+      buildInputs = previousAttrs.buildInputs ++ [
+        libx11.dev
+        libxcb.dev
+        libxkbcommon.dev
+        libxtst
+      ];
+      desktopItems = lib.optional stdenv.hostPlatform.isLinux (makeDesktopItem {
+        name = "dev.vencord.Vesktop";
+        desktopName = "Vesktop";
+        exec = "vesktop %U";
+        icon = "vesktop";
+        startupWMClass = "vesktop";
+        genericName = "Internet Messenger";
+        startupNotify = false;
+        keywords = [
+          "discord"
+          "vencord"
+          "electron"
+          "chat"
+        ];
+        categories = [
+          "Network"
+          "InstantMessaging"
+          "Chat"
+        ];
+      });
+    }))
   ];
 
   scarey.home = {
@@ -46,7 +86,7 @@
         "org.gnome.Nautilus.desktop"
         "firefox.desktop"
         "com.mitchellh.ghostty.desktop"
-        "vesktop.desktop"
+        "dev.vencord.Vesktop.desktop"
         "steam.desktop"
       ];
     };
