23739590d4ff52acc31ce3fb17d6170525719f5b
Samuel Carey <sam@scarey.me>
2026-04-01 20:24:36 -0400

    feat: install steam

--- hosts/teseuka/default.nix
+++ hosts/teseuka/default.nix
@@ -98,6 +98,9 @@
     binfmt = true;
   };
 
+  # Enable Steam
+  programs.steam.enable = true;
+
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
